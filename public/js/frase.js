bbdadc7c46ee7ec462edddf0a76eb8ca6bfabfc5
Crummorn <igorcaze@gmail.com>
2019-09-11 12:04:29 -0300

    Enviando dados com AJAX

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,8 @@
 $("#js-botao-frase").click(fraseAleatoria);
+$("#js-botao-frase-id").click(fraseEspecifica);
 
 function fraseAleatoria() {
-    $("#js-spinner").show();
+    $("#js-spinner").toggle();
 
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
     .fail(function() {
@@ -12,7 +13,7 @@ function fraseAleatoria() {
         }, 3000);
     })
     .always(function () {
-        $("#js-spinner").hide();
+        $("#js-spinner").toggle();
     });
 }
 
@@ -23,4 +24,31 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numAleatorio].texto);   
     atualizaTamanhoFrase();    
     atualizaTempoInicial(data[numAleatorio].tempo);
+}
+
+function fraseEspecifica() {
+    $("#js-spinner").toggle();
+
+    var fraseId = $("#js-frase-id").val();
+    var dados = {id : fraseId}; //criacao do objeto JS que guarda a id
+
+    //passando objeto como segundo parâmetro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function() {
+        $("#js-error").show();
+
+        setTimeout(function () {
+            $("#js-error").hide();
+        }, 3000);
+    })
+    .always(function () {
+        $("#js-spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+    var frase = $(".js-frase");
+    frase.text(data.texto); 
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
